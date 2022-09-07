f1c490b764c79229ba7520d9a94da83dc65af128
cirius1792 <cirolucio.tecce@gmail.com>
2022-09-07 18:47:07 +0200

    first commit

---
+++ src/test/java/com/aidexa/assessment/AssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aidexa.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
